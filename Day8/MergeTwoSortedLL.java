8c2537fa7ec84e48278f4f34c24f1db7ec8518dd
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2022-05-23 12:22:24 +0530

    Create MergeTwoSortedLL.java

---
+++ Day8/MergeTwoSortedLL.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode head = new ListNode();
+        ListNode dummy = head;
+        
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                head.next = list1;
+                list1 = list1.next;
+            } else {
+                head.next = list2;
+                list2 = list2.next;
+            }
+            head = head.next;
+        }
+        
+        if(list1!= null) {
+            head.next = list1;
+        } 
+        if(list2 != null) {
+            head.next = list2;
+        }
+        
+        return dummy.next;
+        
+    }
+}
+
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+
+
+// Here we will create a dummy node and and we will keep on adding the smaller element to its next unless we find a smaller element from ll1 and ll2
+
